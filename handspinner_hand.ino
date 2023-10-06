ddd6c782db77bce06be6c39722d4c9e990aaabab
Shintaro Tamefusa <tameshin@gmail.com>
2023-10-07 00:28:09 +0900

    add motor control

--- handspinner_hand.ino
+++ handspinner_hand.ino
@@ -1,10 +1,24 @@
+#include <Servo.h>
+
+Servo hand, spinner;
+
 int val = 0, pre_val = 0;
 int threshould = 300;
-int count = 0;
+int count = 0, tmp_count = 0;
 unsigned long start = 0;
+int state = 0;  // 0:停止 1:回転中 2:スピナー回転準備 3:手で回転させる 4:スピナー戻す 5:手戻す
 
 void setup() {
   Serial.begin(9600);
+  Serial.setTimeout(10);  // SerialでのString受信のタイムアウト設定（ms）
+
+  // サーボピン初期化
+  hand.attach(7);
+  spinner.attach(8);
+
+  hand.write(10);
+  spinner.write(170);
+
   start = millis();
 }
 
@@ -17,9 +31,33 @@ void loop() {
   }
   if (millis() - start > 1000) {
     Serial.println(count);
+    if (count >= 3 && state == 0) {
+      state = 1;
+      Serial.println("START");
+    } else if (count < 3 && state == 1) {
+      state = 2;
+      Serial.println("SLOW DOWN");
+    } else if (state == 2) {
+      state = 3;
+      Serial.println("ROTATE SPINNER TO SPIN");
+      spinner.write(10);
+    } else if (state == 3) {
+      Serial.println("ROTATE HAND TO SPIN");
+      state = 4;
+      hand.write(170);
+    } else if (state == 4) {
+      Serial.println("RETURN SPINNER");
+      state = 5;
+      spinner.write(170);
+    } else if (state == 5) {
+      Serial.println("RETURN HAND");
+      state = 0;
+      hand.write(10);
+    }
     count = 0;
     start = millis();
   }
   pre_val = val;
+
   delay(1);
 }
